2b24612e3af886ac7cda8f7ad2023d74911df5f4
ayushsatii07 <ayushsati844@gmail.com>
2026-03-02 14:20:07 +0530

    UC9: Recursive Palindrome Checker

--- B1P26_RA2411003010532/src/PalindromeCheckerApp.java
+++ B1P26_RA2411003010532/src/PalindromeCheckerApp.java
@@ -1,32 +1,19 @@
-import java.util.Deque;
-import java.util.ArrayDeque;
-
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-
-        String input = "refer";
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char c : input.toCharArray()) {
-            deque.addLast(c);
-        }
-
-        boolean isPalindrome = true;
-
-        while (deque.size() > 1) {
-
-            char first = deque.removeFirst();
-            char last = deque.removeLast();
-
-            if (first != last) {
-                isPalindrome = false;
-                break;
-            }
-        }
-
+        String input = "madam";
+        boolean isPalindrome = check(input, 0, input.length() - 1);
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+        return check(s, start + 1, end - 1);
     }
 }
