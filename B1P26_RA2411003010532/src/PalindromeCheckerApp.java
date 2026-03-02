bfc7e0adbf66308f8eff235e91c01938c9c7c3c1
Soumyakanta-05 <dattaguptasoumyakanta@gmail.com>
2026-03-02 11:43:41 +0530

    UC1:Welcome Message

---
+++ B1P26_RA2411003010532/src/PalindromeCheckerApp.java
@@ -0,0 +1,14 @@
+public class PalindromeCheckerApp {
+
+    public static class UseCase1PalindromeApp {
+        public UseCase1PalindromeApp() {
+            System.out.println("Welcome to Palindrome Checker Management System");
+            System.out.println("Version 1.0");
+            System.out.println("System initialised successfully");
+        }
+    }
+    public static void main(String[] args) {
+        UseCase1PalindromeApp entry = new UseCase1PalindromeApp();
+    }
+
+}
