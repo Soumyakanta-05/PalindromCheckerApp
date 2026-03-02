9018f8cb22bdcb234aa77c7991740b65a4397ed0
Soumyakanta-05 <dattaguptasoumyakanta@gmail.com>
2026-03-02 15:07:24 +0530

    UC10:Case-Insensitive & Space-Ignored Palindrome

--- B1P26_RA2411003010532/src/PalindromeCheckerApp.java
+++ B1P26_RA2411003010532/src/PalindromeCheckerApp.java
@@ -9,7 +9,30 @@ public class PalindromeCheckerApp {
 
         Deque<Character> deque = new ArrayDeque<>();
 
-        for (char c : input.toCharArray()) {
+        for (char c : input.toCharArrimport java.util.Scanner;
+
+        public class PalindromeCheckerApp {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                System.out.print("Enter Input : ");
+                String input = sc.nextLine();
+                String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+                boolean isPalindrome = true;
+                for (int i = 0; i < normalized.length() / 2; i++) {
+
+                    if (normalized.charAt(i) !=
+                            normalized.charAt(normalized.length() - 1 - i)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+
+                System.out.println("Input : " + input);
+                System.out.println("Is Palindrome? : " + isPalindrome);
+
+                sc.close();
+            }
+        }ay()) {
             deque.addLast(c);
         }
 
